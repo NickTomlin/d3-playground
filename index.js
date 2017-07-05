e60e8cdf175888355ed2176d16fcaf5359e0f9e8
Nick Tomlin <nick.tomlin@gmail.com>
2017-07-05 18:53:06 -0500

    Simplify

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 import * as d3 from 'd3';
 
-// errmmm dirty hacks to avoid an extra index.html
-let css = `
-.chart div {
+// root SVG element
+d3.select(document.body)
+  .append('svg')
+  .attr('height', 600)
+  .attr('width', 1000)
+
+// dirty hacks to keep everything in one place
+const css = `
+div {
   font: 10px sans-serif;
   background-color: steelblue;
   text-align: right;
@@ -16,39 +22,18 @@ path {
   stroke: #aaa;
 }
 `
+
 let style = document.createElement('style')
 style.textContent = css
 document.body.appendChild(style)
 
+/* == EXAMPLE: delete me */
+
 let data = [30, 86, 168, 281, 303, 365]
-
-let chart = document.createElement('div')
-document.body.appendChild(chart)
-chart.classList.add('chart')
-
-d3.select(chart)
+d3.select(document.body)
   .selectAll('div')
   .data(data)
   .enter()
   .append('div')
   .style('width', (d) => d + 'px')
   .text(d => d)
-
-var lineData = [[0, 50], [100, 80], [200, 40], [300, 60], [400, 30]];
-
-let lineGenerator = d3.line().curve(d3.curveNatural)
-let pathString = lineGenerator(lineData)
-
-d3.select('path')
-  .attr('d', pathString)
-
-var someData= ["filler", "filler", "filler", "filler"];
-d3.select("body").selectAll("#borderdiv")
-  .data(someData)
-  .enter()
-  .append("div")
-  .html("Wow")
-
-  .append("span")
-  .html("Even yow Wow")
-  .style("font-weight", "900");
